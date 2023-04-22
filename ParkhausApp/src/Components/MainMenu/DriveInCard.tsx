5f0dc1a6a985a886eab14596991be35c14bfb157
Gerrit Koppe <gkoppe@sievers-group.com>
2023-04-22 21:19:17 +0200

    Added more functionality

--- ParkhausApp/src/Components/MainMenu/DriveInCard.tsx
+++ ParkhausApp/src/Components/MainMenu/DriveInCard.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import FormLabel from 'react-bootstrap/FormLabel';
 import Form from 'react-bootstrap/Form'
 
-export default function DriveInCard(tempProps?: {licPlateClassName?: string, buttonClickHandler: () => boolean, changeListener?: (plate: string) => void}) {
+export default function DriveInCard(tempProps?: {licPlateClassName?: string, buttonClickHandler: () => boolean, changeListener?: (plate: string, perma: boolean) => void}) {
     const props = {
         licPlateClassName: 'defaultInput',
         buttonClickHandler: () => {return true;},
@@ -14,13 +14,19 @@ export default function DriveInCard(tempProps?: {licPlateClassName?: string, but
     }
 
     const [licPlate, setLicPlate] = useState('');
+    const [perma, setPerma] = useState(true);
 
     const changeListener = ({target} : { target: {value: string}}) => {
         let newLicPlate = target.value.toUpperCase();
         setLicPlate(newLicPlate);
-        props.changeListener(newLicPlate);
+        props.changeListener(newLicPlate, perma);
     }
 
+    const permaChange = () => {
+        setPerma((prev) => !prev);
+        props.changeListener(licPlate, perma);
+    }
+    
     const buttonClickHandler = () => {
         if (props.buttonClickHandler()) {
             setLicPlate('');
@@ -29,9 +35,9 @@ export default function DriveInCard(tempProps?: {licPlateClassName?: string, but
 
     return (
         <div className="driveInCard">
-            <FormLabel>NummernSchild:</FormLabel><br/><input value={licPlate} onChange={changeListener} className={props.licPlateClassName}/>
+            <FormLabel>Nummernschild:</FormLabel><br/><input value={licPlate} onChange={changeListener} className={props.licPlateClassName}/>
             <br/>
-            <FormLabel>Dauerparker:</FormLabel><Form.Check type="switch" id="perma-parker"/>
+            <FormLabel>Dauerparker:</FormLabel><Form.Check type="switch" id="perma-parker" checked={perma} onChange={permaChange}/>
             <br/>
             <div>
                 <DriveInButton clickHandler={buttonClickHandler}/>
--- ParkhausApp/src/Components/MainMenu/MainMenu.tsx
+++ ParkhausApp/src/Components/MainMenu/MainMenu.tsx
@@ -4,36 +4,45 @@ import { useState } from 'react';
 
 function MainMenu() {
     const [usage, setUsage] = useState(0);
+    const [permaParkers, setpermaParkers] = useState(0);
     const [licClass, setlicClass] = useState('defaultInput');
     let licPlate = '';
+    let permaParker = false;
 
     const driveInClickHandler = () => {
-        const licChecker = /[A-ZÖÜÄ]{1,3}-[A-ZÖÜÄ]{1,2}-[1-9]{1}[0-9]{1,3}[EH]{1}/;
+        const licChecker = /[A-ZÖÜÄ]{1,3}-[A-ZÖÜÄ]{1,2}-[1-9]{1}[0-9]{1,3}[EH]{0,1}/;
         if (!licChecker.test(licPlate)) {
             setlicClass('error');
             return false;
         } else {
             usage + 1 > 180? setUsage(usage) : setUsage((prev) => prev +1);
+            permaParker ? setpermaParkers((prev) => prev + 1) : setpermaParkers((prev) => prev);
             setlicClass('defaultInput');
             // TODO Hier muss die tatsächliche Einfahrt geschehen.
             return true;
         }
     }
 
-    const licChangeListener = (plate: string) => {
+    const licChangeListener = (plate: string, perma: boolean) => {
         licPlate = plate;
+        permaParker = perma;
     }
 
 
     return (
-        <div className="menuContainer" style={{width: "100%"}}>
-            <div style={{width: "30%"}}>
-                <DriveInCard buttonClickHandler={driveInClickHandler} licPlateClassName={licClass} changeListener={licChangeListener}/>
+        <>
+            <div style={{width: "50%", textAlign: "center"}}>
+                <h1>Parkhaus Osnabrück</h1>
             </div>
-            <div className="infoSide" style={{width: "25%"}}>
-                <UsageCard usedSpaces={usage}/>
+            <div className="menuContainer" style={{width: "100%"}}>
+                <div className='mainSide'>
+                    <DriveInCard buttonClickHandler={driveInClickHandler} licPlateClassName={licClass} changeListener={licChangeListener}/>
+                </div>
+                <div className="infoSide">
+                    <UsageCard usedSpaces={usage} permaParkers={permaParkers}/>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
