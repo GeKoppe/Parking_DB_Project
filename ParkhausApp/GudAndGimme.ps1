a22f2377498b7da6fce83a215a413903a83e0621
Gerrit Koppe <gkoppe@sievers-group.com>
2023-04-23 15:38:37 +0200

    Testing automatic commit

---
+++ ParkhausApp/GudAndGimme.ps1
@@ -0,0 +1,10 @@
+﻿$folder = $args[0]
+$message = $args[1]
+
+if ($folder -and $message) {
+    git add $folder
+    git commit -m $message
+    git push
+} else {
+    Write-Host("Folder or message missing")
+}
