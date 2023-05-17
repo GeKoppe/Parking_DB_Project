23f9dd91c2fe3cc70a015b34221ebead6dd251d6
Gerrit Koppe <gkoppe@sievers-group.com>
2023-05-17 14:13:25 +0200

    Prettier doing it's thing

--- ParkhausApp/src/App.tsx
+++ ParkhausApp/src/App.tsx
@@ -1,51 +1,71 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
-import MainMenu from './Components/MainMenu/MainMenu'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import MainMenu from './Components/MainMenu/MainMenu';
 import Administration from './Components/Administration/Administration';
 import Navbar from './Components/NavBar';
-import './App.css'
+import './App.css';
 import Registration from './Components/Registration/Registration';
+import History from './Components/History/History';
 
 function App() {
-  const [currentMenu, setCurrentMenu] = useState('MainMenu');
+	const [currentMenu, setCurrentMenu] = useState('MainMenu');
 
-  const clickHandler = (site: string) => {
-    switch (site) {
-      case 'home': setCurrentMenu('MainMenu'); break;
-      case 'admin': setCurrentMenu('Administration'); break;
-      case 'registration': setCurrentMenu('Registration'); break;
-      default: setCurrentMenu('MainMenu'); break;
-    }
-    return;
-  }
+	const clickHandler = (site: string) => {
+		switch (site) {
+			case 'home':
+				setCurrentMenu('MainMenu');
+				break;
+			case 'admin':
+				setCurrentMenu('Administration');
+				break;
+			case 'registration':
+				setCurrentMenu('Registration');
+				break;
+			case 'history':
+				setCurrentMenu('History');
+				break;
+			default:
+				setCurrentMenu('MainMenu');
+				break;
+		}
+		return;
+	};
 
-  const mockUsage = Math.ceil(Math.random() * 180);
-  let lotsInUse : number[] = [];
-  for (let i = 0; i < mockUsage; i++) {
-      let selected = Math.ceil(Math.random() * 180);
+	const mockUsage = Math.ceil(Math.random() * 180);
+	let lotsInUse: number[] = [];
+	for (let i = 0; i < mockUsage; i++) {
+		let selected = Math.ceil(Math.random() * 180);
 
-      while (true) {
-          let isInUse = true;
-          for (let j = 0; j < lotsInUse.length; j++) {
-              if (selected === lotsInUse[j]) {
-                  isInUse = true;
-                  break;
-              }
-          }
+		while (true) {
+			let isInUse = true;
+			for (let j = 0; j < lotsInUse.length; j++) {
+				if (selected === lotsInUse[j]) {
+					isInUse = true;
+					break;
+				}
+			}
 
-          if (isInUse) {
-              lotsInUse.push(i);
-              break;
-          }
-      }
-  }
+			if (isInUse) {
+				lotsInUse.push(i);
+				break;
+			}
+		}
+	}
 
-  return (
-    <div>
-      <Navbar clickHandler={clickHandler}/>
-      {currentMenu == 'MainMenu' ? <MainMenu /> : (currentMenu == 'Administration' ? <Administration/> : <Registration/>)}
-    </div>
-  )
+	return (
+		<div>
+			<Navbar clickHandler={clickHandler} />
+			{currentMenu == 'MainMenu' ? (
+				<MainMenu />
+			) : currentMenu == 'Administration' ? (
+				<Administration />
+			) : currentMenu == 'History' ? (
+				<History />
+			) : (
+				<Registration />
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
--- ParkhausApp/src/Components/NavBar.tsx
+++ ParkhausApp/src/Components/NavBar.tsx
@@ -1,33 +1,60 @@
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
-import '../App.css'
+import '../App.css';
 
-function NavBar(tempProps?: {clickHandler?: (site: string) => void}) {
-    const props = {
-        clickHandler: (site: string) => console.log(site),
-        ...tempProps
-    }
+function NavBar(tempProps?: { clickHandler?: (site: string) => void }) {
+	const props = {
+		clickHandler: (site: string) => console.log(site),
+		...tempProps,
+	};
 
-    return (
-        <div className="navBar">
-            <Navbar expand="lg">
-                <Container>
-                    <Navbar.Brand>Parkhaus Verwaltung</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Button className='navButton' variant="custom" onClick={() => props.clickHandler('home')}>Startseite</Button>
-                        <Button className='navButton' variant="custom" onClick={() => props.clickHandler('registration')}>Registrierung</Button>
-                        <Button className='navButton' variant="custom" onClick={() => props.clickHandler('admin')}>Administration</Button>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    )
+	return (
+		<div className="navBar">
+			<Navbar expand="lg">
+				<Container>
+					<Navbar.Brand>Parkhaus Verwaltung</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Button
+								className="navButton"
+								variant="custom"
+								onClick={() => props.clickHandler('home')}
+							>
+								Startseite
+							</Button>
+							<Button
+								className="navButton"
+								variant="custom"
+								onClick={() =>
+									props.clickHandler('registration')
+								}
+							>
+								Registrierung
+							</Button>
+							<Button
+								className="navButton"
+								variant="custom"
+								onClick={() => props.clickHandler('admin')}
+							>
+								Administration
+							</Button>
+							<Button
+								className="navButton"
+								variant="custom"
+								onClick={() => props.clickHandler('history')}
+							>
+								Geschichte
+							</Button>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</div>
+	);
 }
 
 export default NavBar;
