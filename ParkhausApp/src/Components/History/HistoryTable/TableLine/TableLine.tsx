719627a374c4c27e978d2abcf4ecda086e178e48
Gerrit Koppe <gkoppe@sievers-group.com>
2023-05-17 21:00:52 +0200

    Started on making history table

--- ParkhausApp/src/Components/History/HistoryTable/TableLine/TableLine.tsx
+++ ParkhausApp/src/Components/History/HistoryTable/TableLine/TableLine.tsx
@@ -1,14 +1,18 @@
 import { conf } from '../../../../res/config';
+import '../../History.css';
 
-export default function TableLine(tempProps?: any) {
+export default function TableLine(tempProps: { licPlate: string; entryDate: string; outDate: string; cost: string; perma: boolean }) {
 	const props = {
 		...tempProps,
 	};
 
 	return (
-		<tr className="tableLine">
-			<th></th>
-			<th></th>
+		<tr className='tableLine'>
+			<td className='tableItem'>{props.licPlate}</td>
+			<td className='tableItem'>{props.entryDate}</td>
+			<td className='tableItem'>{props.outDate}</td>
+			<td className='tableItem'>{props.cost}</td>
+			<td className='tableItem'>{props.perma ? 'Ja' : 'Nein'}</td>
 		</tr>
 	);
 }
--- ParkhausApp/src/Components/MainMenu/UsageCard/UsageCard.tsx
+++ ParkhausApp/src/Components/MainMenu/UsageCard/UsageCard.tsx
@@ -4,53 +4,49 @@ import SpaceSubCard from './SpaceSubCard';
 import { useState, useEffect } from 'react';
 import { conf } from '../../../res/config';
 
-function UsageCard(tempProps: {renderInfo?: boolean}) {
-    const props = {
-        renderInfo: false,
-        ...tempProps
-    }
-    
-    const [usedSpaces, setUsedSpaces] = useState(0);
-    const [permaParkers, setpermaParkers] = useState(0);
+function UsageCard(tempProps: { renderInfo?: boolean }) {
+	const props = {
+		renderInfo: false,
+		...tempProps,
+	};
 
-    useEffect(() => {
-        fetch(`http://${conf.api.host}:${conf.api.port}/${conf.api.routes.usage}`, {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(data => {
-            return data.json() as Promise<{usage: number, perma: number}>;
-        })
-        .then(data => {
-            setUsedSpaces(data.usage);
-            setpermaParkers(data.perma);
-        });
-    }, [props.renderInfo]);
+	const [usedSpaces, setUsedSpaces] = useState(0);
+	const [permaParkers, setpermaParkers] = useState(0);
 
-    const colorShades : string[] = ['#ffffff', '#ffdddd', '#fcb6b6', '#fca0a0', '#ff8e8e', '#f87474', '#ff5252', '#fd4343', '#fa2828', '#ff0000'];
+	useEffect(() => {
+		fetch(`http://${conf.api.host}:${conf.api.port}/${conf.api.routes.usage}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(data => {
+				return data.json() as Promise<{ usage: number; perma: number }>;
+			})
+			.then(data => {
+				setUsedSpaces(data.usage);
+				setpermaParkers(data.perma);
+			});
+	}, [props.renderInfo]);
 
-    let usage : number = usedSpaces / 180;
-    const tColor : string = colorShades[(Math.floor(usage*10) > 9 ? 9 : Math.floor(usage*10))];
+	const colorShades: string[] = ['#ffffff', '#ffdddd', '#fcb6b6', '#fca0a0', '#ff8e8e', '#f87474', '#ff5252', '#fd4343', '#fa2828', '#ff0000'];
 
-    return (
-        <div className="usageCard">
-            <h4>Belegung</h4>
-            <br/>
-            <SpaceSubCard freeSpaces={usedSpaces} textColor={tColor}/>
-            <br/>
-            <h4 className='spaceCard'>von 180</h4>
-            <br/>
-            <h5>
-                Auslastung: {parseInt("" + (usage * 100))}%
-            </h5>
-            <br/>
-            <h5>
-                Dauerparker: {permaParkers}
-            </h5>
-        </div>
-    )
+	let usage: number = usedSpaces / 180;
+	const tColor: string = colorShades[Math.floor(usage * 10) > 9 ? 9 : Math.floor(usage * 10)];
+
+	return (
+		<div className='usageCard'>
+			<h4>Belegung</h4>
+			<br />
+			<SpaceSubCard freeSpaces={usedSpaces} textColor={tColor} />
+			<br />
+			<h4 className='spaceCard'>von 180</h4>
+			<br />
+			<h5>Auslastung: {parseInt('' + usage * 100)}%</h5>
+			<br />
+			<h5>Dauerparker: {permaParkers}</h5>
+		</div>
+	);
 }
 
 export default UsageCard;
