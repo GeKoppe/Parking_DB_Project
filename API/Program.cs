9c1f41f17948b396ea5752ca875c2cefb988cceb
Jannik Glane <jannikglane@posteo.de>
2023-05-17 14:47:02 +0200

    chore: allow cors

--- API/Program.cs
+++ API/Program.cs
@@ -3,13 +3,17 @@ using API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<DbContext>();
+
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
