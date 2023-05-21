397051d660af1355245f6136c7ea238f0a9b29b3
Gerrit Koppe <gkoppe@sievers-group.com>
2023-05-21 14:05:27 +0200

    More integration

--- ParkhausApp/src/Components/MainMenu/MainMenu.tsx
+++ ParkhausApp/src/Components/MainMenu/MainMenu.tsx
@@ -14,37 +14,23 @@ function MainMenu() {
 	};
 
 	const driveInClickHandler = (plate: string, perma: boolean) => {
-		const licChecker =
-			/[A-ZÖÜÄ]{1,3}-[A-ZÖÜÄ]{1,2}-[1-9]{1}[0-9]{1,3}[EH]{0,1}/;
+		const licChecker = /[A-ZÖÜÄ]{1,3}-[A-ZÖÜÄ]{1,2}-[1-9]{1}[0-9]{1,3}[EH]{0,1}/;
 		if (!licChecker.test(plate) || plate.length > 12) {
 			setlicClass('error');
 			return false;
 		}
 
-		if (plate === 'RI-CK-1337')
-			window.open(
-				'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				'_blank',
-				'noreferrer'
-			);
-		if (plate === 'SMÜ-GE-17')
-			window.open(
-				'https://www.google.com/maps/place/@48.3924956,10.8350031,17z/data=!3m2!4b1!5s0x479ebc94c8aa18e3:0x1b232ca59fd40517!4m6!3m5!1s0x479ebc94c8c512b9:0x3cee8fdba6a91970!8m2!3d48.3924956!4d10.8350031!16s%2Fg%2F1q2vx2c0f',
-				'_blank',
-				'noreferrer'
-			);
+		if (plate === 'RI-CK-1337') window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank', 'noreferrer');
+		if (plate === 'SMÜ-GE-17') window.open('https://www.google.com/maps/place/@48.3924956,10.8350031,17z/data=!3m2!4b1!5s0x479ebc94c8aa18e3:0x1b232ca59fd40517!4m6!3m5!1s0x479ebc94c8c512b9:0x3cee8fdba6a91970!8m2!3d48.3924956!4d10.8350031!16s%2Fg%2F1q2vx2c0f', '_blank', 'noreferrer');
 
-		fetch(
-			`http://${conf.api.host}:${conf.api.port}${conf.api.routes.newParker}`,
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ plate: plate }),
-			}
-		)
-			.then((response) => {
+		fetch(`http://${conf.api.host}:${conf.api.port}/${conf.api.routes.newParker}?kennzeichen=${plate}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			// body: JSON.stringify({ plate: plate }),
+		})
+			.then(response => {
 				// TODO Error handling, anything other than 200 basically
 				return response.json() as Promise<{
 					id: number;
@@ -53,7 +39,7 @@ function MainMenu() {
 					nachname?: string;
 				}>;
 			})
-			.then((data) => {
+			.then(data => {
 				let welcomeString = 'Herzlich Willkommen';
 				if (data?.dauerparker) {
 					welcomeString += ` ${data?.vorname} ${data?.nachname}`;
@@ -69,23 +55,20 @@ function MainMenu() {
 					theme: 'colored',
 				});
 			});
+		setRenderInfo(!renderInfo);
 		setlicClass('defaultInput');
 		return true;
 	};
 
 	return (
 		<>
-			<div className="menuContainer">
-				<ToastContainer className="welcomeToast" />
-				<div className="mainSide">
-					<DriveInCard
-						buttonClickHandler={driveInClickHandler}
-						licPlateClassName={licClass}
-						licChangeHandler={licChangeHandler}
-					/>
+			<div className='menuContainer'>
+				<ToastContainer className='welcomeToast' />
+				<div className='mainSide'>
+					<DriveInCard buttonClickHandler={driveInClickHandler} licPlateClassName={licClass} licChangeHandler={licChangeHandler} />
 				</div>
-				<div className="vLine"></div>
-				<div className="infoSide">
+				<div className='vLine'></div>
+				<div className='infoSide'>
 					<UsageCard renderInfo={renderInfo} />
 				</div>
 			</div>
